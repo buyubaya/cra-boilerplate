2c0ebd48dab8a777c78891f43553666a6a043f6e
duchieu-le <duchieu.le@navercorp.com>
2022-09-03 22:29:01 +0700

    added Sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Sample from './components/Sample';
 
 function App() {
   return (
     <div className="App">
-      <h1>HELLO</h1>
+      <Sample />
     </div>
   );
 }
