e4b194578e00a30f5982fe2b5d75e1ee8b2a3d87
duchieu-le <duchieu.le@navercorp.com>
2022-09-03 21:53:49 +0700

    hello

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.{js,jsx}': 'npm run lint:eslint',
-  '*.{ts,tsx}': () => 'npm run lint',
+  '*.{js,jsx}': ['npm run prettier:check', 'npm run lint:eslint'],
+  '*.{ts,tsx}': () => ['npm run prettier:check', 'npm run lint'],
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import './App.scss';
 
-function test() {
-  console.log('11111 =>', 11111);
-}
-
 function App() {
   return (
     <div className="App">
