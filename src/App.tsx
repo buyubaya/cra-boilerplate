140634a0d8a2f9ae926f57fbe800df4556135aa5
duchieu-le <duchieu.le@navercorp.com>
2022-09-04 00:39:40 +0700

    added redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { Counter } from '@/components/Counter/Counter';
 import Sample from '@/components/Sample';
 import React from 'react';
+import { Provider } from 'react-redux';
 import './App.scss';
+import { store } from './redux/store';
 
 function App() {
   return (
-    <div className="App">
-      <Sample />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Sample />
+        <Counter />
+      </div>
+    </Provider>
   );
 }
 
